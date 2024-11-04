43db70acb50d35731b77eb4c384e7b9fc8043788
Tim Arnold <26187756+degenerateddev@users.noreply.github.com>
2024-11-04 03:42:37 +0100

    Add: initial routes

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -3,4 +3,21 @@
 	let { children } = $props();
 </script>
 
-{@render children()}
+<main>
+	<div class="flex">
+		<div class="flex flex-col">
+			<a href="/" class="text-2xl font-bold">New</a>
+		</div>
+
+		<div class="flex-grow">
+			<div class="flex flex-col justify-between">
+				<div class="flex items-center justify-center gap-5">
+					<a href="/" class="text-2xl font-bold">Home</a>
+					<a href="/" class="text-2xl font-bold">Auth</a>
+				</div>
+				
+				{@render children()}
+			</div>
+		</div>
+	</div>
+</main>
