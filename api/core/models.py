ce5a435d1d69c069fd0b7bc5f2e732e3bd9a7587
Tim <tim@egirl.ceo>
2024-11-04 13:29:27 +0100

    Add: base models

--- api/core/models.py
+++ api/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Text(models.Model):
+    content = models.CharField(max_length=5000)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Dialogue(Text):
+    pass
+
+class Repetitive(Text):
+    pass
--- api/core/views.py
+++ api/core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+class Dialogue(APIView):
+    def get(self, request):
+        return Response({"message": "Dialogue GET request"})
+
+    def post(self, request):
+        return Response({"message": "Dialogue POST request"})
