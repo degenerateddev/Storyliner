59dfdb5452cf96b39880bdd0081a91d9ef6cc2d6
Tim <tim@egirl.ceo>
2024-11-25 13:22:15 +0100

    Add: hardlock to writing page if not logged in

--- client/src/hooks.server.js
+++ client/src/hooks.server.js
@@ -1,6 +1,13 @@
+import { redirect } from '@sveltejs/kit';
+
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
-	
+	if (event.url.pathname !== "/auth") {
+		const cookies = event.cookies;
+		if (!(cookies.get("access") && cookies.get("refresh"))) {
+			return redirect(303, "/auth");
+		}
+	}
 
 	const response = await resolve(event);
 	return response;
